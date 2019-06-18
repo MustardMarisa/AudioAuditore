8cb0ca552ae7d8a2136bf1c14518fb0c0af5bda5
Carlos Binderbul <calva.cartablanca@gmail.com>
2019-06-18 18:17:40 -0500

    Implementacion de una peticion con axios

--- audio-auditore-front/src/components/Contact.js
+++ audio-auditore-front/src/components/Contact.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Contact extends Component {
+
+    state = {
+        nombre: null
+    }
+
+    getCharacter = (e) => {
+        e.preventDefault();
+        const personaje = e.target.elements.personaje.value;
+        if (personaje) {
+            axios.get(`https://swapi.co/api/people/${personaje}`)
+                .then((res) => {
+                    const nombre = res.data.name;
+                    this.setState({ nombre });
+                })
+        } else return;
+    }
+
     render() {
         return (
             <div>
-                <h2>GOT QUESTIONS?</h2>
-                <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
-        </p>
+                <h2>Ingresa un numero para buscar un personaje de la SWAPI</h2>
+                <form onSubmit={this.getCharacter}>
+                    <input type="text" name="personaje" />
+                    <button>Submit</button>
+                </form>
+                { this.state.nombre ? <p>Nombre del personaje: { this.state.nombre }</p> : <p>Porfavor introduce un numero.</p> }
             </div>
         );
     }
